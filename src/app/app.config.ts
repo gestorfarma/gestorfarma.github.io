36ab0de2d66d539d0b5a82c44d32af4860fa781d
brunojoao <bruno.joao@triersistemas.com.br>
2025-08-21 14:39:26 -0300

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,17 +3,21 @@ import {
   ApplicationConfig,
   importProvidersFrom,
   provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
+  provideZonelessChangeDetection, isDevMode,
 } from '@angular/core';
 import { provideRouter, withRouterConfig } from '@angular/router';
 import { PlotlyModule } from 'angular-plotly.js';
 import * as PlotlyJS from 'plotly.js-dist-min';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withRouterConfig({ paramsInheritanceStrategy: 'always' })),
-    importProvidersFrom(PlotlyModule.forRoot(PlotlyJS)),
+    importProvidersFrom(PlotlyModule.forRoot(PlotlyJS)), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
